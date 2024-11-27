c1eaa436e1b5df1583195b2e388d8d5fd08e6222
proxy <aittaleb108@gmail.com>
2024-11-27 14:42:44 +0100

    add new comapani if not found

--- src/app/(chor)/companies/components/companies.tsx
+++ src/app/(chor)/companies/components/companies.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getCompanies } from "../actions/company";
 import Pagination from "./paginations";
 import CompanyCard from "./company-card";
+import Link from "next/link";
 
 export default async function Companies({
   query,
@@ -15,9 +16,21 @@ export default async function Companies({
   return (
     <div className="my-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {companies.map((company) => (
-          <CompanyCard key={company.id} company={company} />
-        ))}
+        {companies.length ? (
+          companies.map((company) => (
+            <CompanyCard key={company.id} company={company} />
+          ))
+        ) : (
+          <div className="text-muted-foreground text-center w-full">
+            No companies found add a new company
+            <Link
+              href="/companies/new"
+              className="text-primary hover:underline ml-2"
+            >
+              Here
+            </Link>
+          </div>
+        )}
       </div>
       <div className="mt-5 flex w-full justify-center">
         <Pagination totalPages={totalPages} />
